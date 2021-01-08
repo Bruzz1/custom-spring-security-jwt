4a9b4801a5e861389cdc3576b0c78a9eff7b7f49
Bruce <bruce@gasc.com>
2021-01-08 17:00:42 +0800

    authentication config

--- src/main/java/com/brux/springsecurityjwtexample/controller/WelcomeController.java
+++ src/main/java/com/brux/springsecurityjwtexample/controller/WelcomeController.java
@@ -1,14 +1,57 @@
 package com.brux.springsecurityjwtexample.controller;
 
+import com.brux.springsecurityjwtexample.model.AuthenticationRequest;
+import com.brux.springsecurityjwtexample.service.CustomUserDetailsService;
+import com.brux.springsecurityjwtexample.util.JwtUtil;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class WelcomeController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
 
 	@GetMapping("/")
 	public String welcomePage() {
 		return "Welcome to my custom spring security updated";
 	}
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+		try {
+			authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+				);
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+		final String jwt = jwtUtil.generateToken(userDetails);
+
+		return ResponseEntity.status(HttpStatus.OK).body(jwt);
+	}
+
+
 	
 }
